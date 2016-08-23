38d3af705a5b3cb32037cf4787567b23952b2213
damsSanch <damian2gom@gmail.com>
2016-08-23 15:29:49 -0500

    Menu(responsive,active,waypoint)

--- js/_scroll.js
+++ js/_scroll.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 600);
-        return false;
-      }
-    }
-  });
+  // $('a[href*="#"]:not([href="#"])').click(function() {
+  //   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  //     var target = $(this.hash);
+  //     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  //     if (target.length) {
+  //       $('html, body').animate({
+  //         scrollTop: target.offset().top
+  //       }, 600);
+  //       return false;
+  //     }
+  //   }
+  // });
 
   var video = $("#player").attr("src");
 
@@ -31,18 +31,82 @@ $(document).ready(function() {
 
   });
 
+  // Scroll - Anclas - Menu con clase active
+  $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 600, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+
+  // Waypoint - Menu, cambiar el color de bg
+  $('.index-bg').waypoint(function(direction) {
+    if(direction == 'down'){
+      $('.center-contents').addClass('center-contents-bg');
+    }
+    else{
+      $('.center-contents').removeClass('center-contents-bg');
+    }
+  }, {
+    offset: '90%'
+  });
+
+  $('.section-bg').waypoint(function(direction) {
+    if(direction == 'down'){
+      $('.center-contents').addClass('center-contents-bg');
+    }
+    else{
+      $('.center-contents').removeClass('center-contents-bg');
+    }
+  }, {
+    offset: '70%'
+  });
 
 });
 
+// Scroll - Menu con clase active
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+    $('.page-nav a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.page-nav ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
 
-// DECLARACIÓN DE LA FUNCION
+// Funcion para mostar/ocultar menu responsive
 function toggleNavigation(){
   $('.page-header').toggleClass('menu-expanded');
   $('.page-nav').toggleClass('collapse');
-  $('.center-contents').toggleClass('center-contents-toggle')
+  $('.center-contents').toggleClass('center-contents-toggle', 'switch');
+  $('.linea1').toggleClass('overL1');
+  $('.linea2').toggleClass('overL2');
+  $('.linea3').toggleClass('overL3');
 }
 
-// EVENTOS DEL DOM
+// DOM - Click en menu responsive
 $(window).on('load',function(){
   $('.toggle-nav').click(toggleNavigation);
 });
